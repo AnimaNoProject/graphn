ea83f33b19776646ee3b038dddd8db03d9618717
Thorsten <thorsten.korpitsch@gmail.com>
2023-03-12 15:22:59 +0100

    provide osb where r is given

--- python/graphn/graphn.py
+++ python/graphn/graphn.py
@@ -328,25 +328,24 @@ def ggd(G_a: nx.Graph, G_b: nx.Graph, dist_func, c_n=1.0, c_e=1.0, verbose=False
     return abs(result)
 
 
-def optimal_sequence_bijection(seq_a, seq_b, C: Optional[float] = None):
+def optimal_sequence_bijection_rij(r_ij, C: Optional[float] = None):
     """
     @brief Computes the optimal sequence bijection according
     Latecki et al. Optimal Subsequence Bijection
-    @param seq_a: Sequence a (needs to be shorter or equal to b)
-    @param seq_b: Sequence b (needs to be longer or equal to a)
+    @param r_ij: Matrix defined by the sequences A and B
     @param C: Optional jump cost, if none is given it is automatically computed based on the input
     @return: Optimal sequence bijection
     """
-    m = len(seq_a)
-    n = len(seq_b)
+    m = np.shape(r_ij)[0]
+    n = np.shape(r_ij)[1]
     assert (m <= n)
 
     g = nx.DiGraph()
-    r = np.zeros((m, n)) * np.inf
+    r = np.ones((m, n))
 
     for i in range(m):
         for j in range(n):
-            r[i][j] = abs(seq_a[i] - seq_b[j])
+            r[i][j] = abs(r_ij[i, j])
             g.add_node("{},{}".format(i, j))
 
     jump_cost = 1.0
@@ -365,7 +364,7 @@ def optimal_sequence_bijection(seq_a, seq_b, C: Optional[float] = None):
                         dst = "{},{}".format(k, l)
                         r[i][j] = np.sqrt((k - i - 1) ** 2
                                           + (l - j - 1) ** 2) * jump_cost \
-                                  + (seq_a[i] - seq_b[j]) ** 2
+                                  + (r_ij[i, j]) ** 2
                         r[i][j] = round(r[i][j], 3)
                         g.add_edge(src, dst, length=r[i][j])
                     # else: # other nodes have infinite weight therfore are not visitable
@@ -380,7 +379,7 @@ def optimal_sequence_bijection(seq_a, seq_b, C: Optional[float] = None):
             lengths = nx.single_source_dijkstra_path_length(g, src, weight="length")
             del paths[src]
             for dst, path in paths.items():
-                if lengths[dst] <= best_length and len(path) == m:
+                if lengths[dst] <= best_length and len(path) >= len(best_path):
                     best_length = lengths[dst]
                     best_path = path
 
@@ -388,4 +387,25 @@ def optimal_sequence_bijection(seq_a, seq_b, C: Optional[float] = None):
     for match in best_path:
         split = match.split(",")
         map_dict[int(split[0])] = int(split[1])
-    return map_dict
+    return map_dict, best_length
+
+
+def optimal_sequence_bijection(seq_a: list, seq_b: list, C: Optional[float] = None):
+    """
+    @brief Computes the optimal sequence bijection according
+    Latecki et al. Optimal Subsequence Bijection
+    @param seq_a: Sequence a (needs to be shorter or equal to b)
+    @param seq_b: Sequence b (needs to be longer or equal to a)
+    @param C: Optional jump cost, if none is given it is automatically computed based on the input
+    @return: Optimal sequence bijection
+    """
+    m = len(seq_a)
+    n = len(seq_b)
+    assert (m <= n)
+    r = np.zeros((m, n)) * np.inf
+
+    for i in range(m):
+        for j in range(n):
+            r[i][j] = abs(seq_a[i] - seq_b[j])
+
+    return optimal_sequence_bijection_rij(r, C)
